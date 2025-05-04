3acdfe0f67068ac5da6dd7b442e0640a66898d04
Andrew Bradley <100674957@unimail.derby.ac.uk>
2025-05-04 12:37:31 +0100

    Removed POST and accounts modules

--- RMS/core/views.py
+++ RMS/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from .models import MenuItem  # Import your model
 
@@ -13,16 +13,11 @@ def menu(request):
     # Pass the data to the template
     return render(request, 'core/menu.html', {'menu_items': menu_items})
 
-def portal_login(request):
+def portal(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            return redirect('index')
-        else:
-            messages.error(request, 'Invalid username or password.')
-    return render(request, 'core/portal.html')
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            return redirect("posts:list")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'core/portal.html', {"form": form})
