9b2b53b31475bdd1a4e5066acbd6036eba508f3c
EdwardOpenSource <edwardbhodhojnr@gmail.com>
2025-05-04 19:01:41 +0100

    Added UserProfile model and admin registration with role management

--- RMS/core/models.py
+++ RMS/core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db import models
 
 # Create your models here.
 class MenuItem(models.Model):
@@ -14,3 +16,18 @@ class MenuItem(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ROLE_CHOICES = (
+        ('Waiter', 'Waiter'),
+        ('Manager', 'Manager'),
+        ('Chef', 'Chef'),
+        
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
