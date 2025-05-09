5bf7aaf62a1897a60e677d5116cf5c266aefb28c
Andrew Bradley <100674957@unimail.derby.ac.uk>
2025-05-09 18:55:20 +0100

    Updated contact us (feedback) email validation and error message to be more descriptive

--- RMS/core/forms.py
+++ RMS/core/forms.py
@@ -9,6 +9,25 @@ class FeedbackForm(forms.ModelForm):
         model = Feedback
         fields = ['name', 'email', 'subject', 'message']
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if '@' in email:
+            domain_part = email.split('@')[1]
+        
+            # Ensure there's a period (.) in the domain part
+            if '.' not in domain_part:
+                raise forms.ValidationError('The provided email address is invalid. A domain name must include a dot.')
+            
+            # Check that the domain part is long enough (e.g., "gmail" is fine, "g" is not)
+            domain_without_extension = domain_part.split('.')[0]
+            if len(domain_without_extension) < 2:
+                raise forms.ValidationError('The domain name is too short to be valid.')
+        else:
+            raise forms.ValidationError('The email must contain an "@" symbol.')
+
+        return email
+
 class PortalLoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
